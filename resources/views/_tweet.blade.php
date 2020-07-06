201a8275e10406f910e3d062016cfea575179253
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2020-07-06 12:51:44 +0300

    Design the Timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,19 @@
+<div class="flex p-4 border border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/50"
+            alt=""
+            class="rounded-full mr-2"
+        >
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">
+            John Doe
+        </h5>
+
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio doloribus dolorum, laborum necessitatibus perferendis ratione repudiandae.
+            Adipisci alias aut cumque cupiditate distinctio dolorem, eaque in omnis, soluta veniam voluptate voluptatibus!
+        </p>
+    </div>
+</div>
